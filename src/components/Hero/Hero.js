e140dafe470abc4de951770aed0ca858acee3220
tausifabid12 <tasifabid12@gmail.com>
2023-02-16 18:24:47 +0600

    social icons added and fabicon changed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,7 @@ import pattern3 from '../../assets/pattern-3.svg';
 import pattern4 from '../../assets/pattern-4.svg';
 import bg from '../../assets/bg-090.jpg';
 import { Link } from 'react-scroll';
-
+import { FaLinkedin, FaFacebook, FaGithub } from 'react-icons/fa';
 import Tilt from 'react-parallax-tilt';
 
 const Hero = () => {
@@ -25,12 +25,36 @@ const Hero = () => {
             Full Stack Developer
           </p>
         </div>
-
-        <p className="text-gray-400 text-xs lg:text-lg font-light">
-          I design and code beautiful simple things and I love it. I'm
-          passionate about writing clean and efficient code, optimizing website
-          performance, and ensuring a seamless user experience.
-        </p>
+        <div>
+          <p className="text-gray-400 text-xs lg:text-lg font-light">
+            I design and code beautiful simple things and I love it. I'm
+            passionate about writing clean and efficient code, optimizing
+            website performance, and ensuring a seamless user experience.
+          </p>
+          <div className="flex space-x-3 pt-3">
+            <a
+              href="https://www.facebook.com/tmabid.ab"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebook className="text-2xl text-white" />
+            </a>
+            <a
+              href="https://github.com/tausifabid12"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub className="text-2xl text-white" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/tausif-muftasin-abid/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedin className="text-2xl text-white" />
+            </a>
+          </div>
+        </div>
         <Link
           spy={true}
           smooth={true}
